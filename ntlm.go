ab83b06623fbd88e45f0f16bace65a9de545f54b
Scoty Stobbe <sstobbe@1800contacts.com>
2024-12-02 13:42:02 -0700

    latest changes

--- ntlm.go
+++ ntlm.go
@@ -27,40 +27,34 @@ type NtlmCredentials struct {
 
 // NtlmRequest is the function that will be accessible from K6
 func (n *Ntlm) NtlmRequest(credentials NtlmCredentials, url string, soapAction string, xmlPayload string) (string, error) {
-	// Configure HTTP client with NTLM Transport
 	client := &http.Client{
 		Transport: &httpntlm.NtlmTransport{
 			Domain:   credentials.Domain,
 			User:     credentials.Username,
 			Password: credentials.Password,
-			// Optional RoundTripper, can customize TLS settings here if needed
 			RoundTripper: &http.Transport{
 				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: true, // Be cautious: only use for testing environments
+					InsecureSkipVerify: true, // Optional, only use for testing.
 				},
 			},
 		},
 	}
 
-	// Prepare the request body with the SOAP payload
 	requestBody := bytes.NewBuffer([]byte(xmlPayload))
 	req, err := http.NewRequest("POST", url, requestBody)
 	if err != nil {
 		return "", err
 	}
 
-	// Set appropriate headers for SOAP
 	req.Header.Set("Content-Type", "text/xml;charset=UTF-8")
 	req.Header.Set("SOAPAction", soapAction)
 
-	// Execute the request
 	resp, err := client.Do(req)
 	if err != nil {
 		return "", err
 	}
 	defer resp.Body.Close()
 
-	// Read response body
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
